61e75dc38d2d2064031c6972b375fc4412bf015c
vietphuong97 <vietphuong97.work@gmail.com>
2025-03-11 11:22:39 +0700

    [IMP] morons: send email (PO, invoice paid)

--- local-addons/morons/models/moron_contributor_invoice.py
+++ local-addons/morons/models/moron_contributor_invoice.py
@@ -10,6 +10,7 @@ class MercTransContributorInvoice(models.Model):
                                         compute="_compute_contributor_id", store=True)
     purchase_order_ids = fields.One2many("project.task", 'contributor_invoice_id', string="Purchase Orders",
                                         domain="[('contributor_id.partner_id', '=', partner_id), ('contributor_invoice_id', '=', False)]")
+    PO_str = fields.Char(string="Purchase Orders", compute="_compute_PO_str", compute_sudo=True)
     is_contributor_invoice = fields.Boolean(string="Is Contributor Invoice", compute="_compute_is_contributor_invoice", store=True,
                                             help="Trường kỹ thuật, dùng để phân quyền")
     amount_total_signed_display = fields.Monetary(string='Invoice Total',
@@ -74,6 +75,11 @@ class MercTransContributorInvoice(models.Model):
         else:
             self.invoice_line_ids = False
 
+    @api.depends("purchase_order_ids")
+    def _compute_PO_str(self):
+        for r in self:
+            r.PO_str = ", ".join([po.name for po in r.purchase_order_ids])
+
     def action_register_payment(self):
         """
         Truyền giá trị mặc định, để payment nhận diện đc là đang thanh toán cho hóa đơn này
@@ -115,3 +121,46 @@ class MercTransContributorInvoice(models.Model):
         if self.user_has_groups('morons.group_bod,morons.group_pm,morons.group_accountants'):
             self_sudo = self.sudo()
         return super(MercTransContributorInvoice, self_sudo).button_draft()
+
+    def action_send_invoice_to_contributor(self):
+        """ Open a window to compose an email, with the edi invoice template
+            message loaded by default
+        """
+        self.ensure_one()
+        template = self.env.ref('morons.email_template_contributor_invoice', raise_if_not_found=False)
+        lang = False
+        if template:
+            lang = template._render_lang(self.ids)[self.id]
+        if not lang:
+            lang = get_lang(self.env).code
+        compose_form = self.env.ref('account.account_invoice_send_wizard_form', raise_if_not_found=False)
+        ctx = dict(
+            default_model='account.move',
+            default_res_id=self.id,
+            default_res_model='account.move',
+            default_use_template=bool(template),
+            default_template_id=template and template.id or False,
+            default_composition_mode='comment',
+            mark_invoice_as_sent=True,
+            default_email_layout_xmlid="mail.mail_notification_layout_with_responsible_signature",
+            model_description=self.with_context(lang=lang).type_name,
+            force_email=True,
+            active_ids=self.ids,
+        )
+
+        report_action = {
+            'name': _('Send Invoice'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'account.invoice.send',
+            'views': [(compose_form.id, 'form')],
+            'view_id': compose_form.id,
+            'target': 'new',
+            'context': ctx,
+        }
+
+        if self.env.is_admin() and not self.env.company.external_report_layout_id and not self.env.context.get('discard_logo_check'):
+            return self.env['ir.actions.report']._action_configure_external_report_layout(report_action)
+
+        return report_action
